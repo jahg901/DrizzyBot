fe3459d1a99999a1276a0636fb4524fcc38743ae
Joshua HG <jahg901@gmail.com>
2021-08-09 04:53:17 -0400

    put function into setfrequency

--- src/setfrequency.js
+++ src/setfrequency.js
@@ -1,3 +1,5 @@
+const DrizzyLines = require("../drizzylines.json");
+
 const SetFrequency = ((msg, server) => {
     if (msg.content.substring(1, 9) === "setfreq " && msg.content.length >= 10 && !/[^0-9]+/.test(msg.content.substring(10))) {
         const freq = parseInt(msg.content.substring(10));
@@ -10,8 +12,24 @@ const SetFrequency = ((msg, server) => {
             }
             clearInterval(server.job)
             server.job = setInterval(() => {
-
-            }, ServerInfo[guild.id].freq * 60000);
+                if (server.latestMsg.channel !== null) {
+                    const msgWords = server.latestMsg.content.toLowerCase().split(/[^a-zA-Z0-9]+/);
+                    let messageFound = false;
+                    for (w of shuffle(msgWords)) {
+                        for (l of shuffle(DrizzyLines)) {
+                            if (l.replace(/[^a-zA-Z0-9 ]/g, "").toLowerCase().includes(` ${w} `)) {
+                                server.latestMsg.channel.send(l);
+                                messageFound = true;
+                                break;
+                            }
+                        }
+                        if (messageFound) break;
+                    }
+                    if (!messageFound) {
+                        server.latestMsg.channel.send(DrizzyLines[Math.floor(Math.random() * DrizzyLines.length)]);
+                    }
+                }
+            }, server.freq * 60000);
         }
     }
 });
