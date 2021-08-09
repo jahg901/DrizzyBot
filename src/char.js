80eee79b734d72a16656116ef2cafb164b04de27
Joshua HG <jahg901@gmail.com>
2021-08-09 04:55:01 -0400

    added message response for setcharacter

--- src/char.js
+++ src/char.js
@@ -3,6 +3,7 @@ const SetCharacter = ((msg, server) => {
         const char = msg.content.substring(9);
         if (char !== " " && char !== "\n" && char !== "/") {
             server.char = char;
+            msg.channel.send(`OK, I respond to \`${char}\` now`);
         }
     }
 });
